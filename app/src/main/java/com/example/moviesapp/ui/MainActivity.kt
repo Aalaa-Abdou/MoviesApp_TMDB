364c75ea3e03701730ccd532b87350ab526708b1
Aalaa-Abdou <aalaa.m.abdou@gmail.com>
2020-05-30 04:33:57 +0200

    make a button pressed effect

--- app/src/main/java/com/example/moviesapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/moviesapp/ui/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.moviesapp.ui
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -21,24 +22,20 @@ class MainActivity : AppCompatActivity(),View.OnClickListener {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        callingApi(sortBy)
+
+        popular.setOnClickListener(this)
+        latest.setOnClickListener(this)
+    }
+
+    fun callingApi(sortBy: String){
         MovieRepository.showingData(object :
             MoviesCallBack {
             override fun onMovieLoaded(Movies: List<Movie>) {
                 populateMoviesRecycler(Movies)
             }
         }, sortBy)
-
-
-
-
-
-        popular.setOnClickListener(this)
-        latest.setOnClickListener(this)
-
     }
-
-
-
 
     private fun populateMoviesRecycler(movieList : List<Movie>){
 
@@ -54,9 +51,19 @@ class MainActivity : AppCompatActivity(),View.OnClickListener {
         when(v?.id){
             R.id.popular -> {
                 sortBy="popularity.desc"
+                latest.setTextColor(Color.GRAY)
+                popular.setTextColor(Color.WHITE)
+                view_latest.visibility = View.INVISIBLE
+                view_popular.visibility = View.VISIBLE
+                callingApi(sortBy)
             }
             R.id.latest -> {
                 sortBy="release_date.desc"
+                popular.setTextColor(Color.GRAY)
+                latest.setTextColor(Color.WHITE)
+                view_popular.visibility = View.INVISIBLE
+                view_latest.visibility = View.VISIBLE
+                callingApi(sortBy)
             }
 
         }
